d8fc865e4fa6e17d893ad957894e928f44392a99
Sophia Jennifer Rose <sophia.j.rose@proton.me>
2025-12-01 11:12:27 +0000

    Day 1

---
+++ day1.py
@@ -0,0 +1,45 @@
+import re
+
+def day1part1(file):
+	with open(file, "r") as input:
+		dial = 50
+		numZeroes = 0
+		numPattern = re.compile(r'(\d+)')
+		for line in input:
+			findNum = numPattern.search(line)
+			num = int(findNum.group())
+			if line.startswith("L"):
+				dial = (dial - num) % 100
+			else:
+				dial = (dial + num) % 100
+			if dial == 0:
+				numZeroes += 1
+		print(numZeroes)
+
+def day1part2(file):
+	with open(file, "r") as input:
+		dial = 50
+		numZeroes = 0
+		numPattern = re.compile(r'(\d+)')
+		for line in input:
+			findNum = numPattern.search(line)
+			num = int(findNum.group())
+			if num > 100:
+				numZeroes += num // 100
+				num = num % 100
+			if line.startswith("L"):
+				newDial = dial - num
+				if newDial <= 0 and dial > 0:
+					numZeroes += 1
+				dial = newDial % 100
+			else:
+				newDial = dial + num
+				if newDial >= 100 and dial < 100:
+					numZeroes += 1
+				dial = newDial % 100
+		print(numZeroes)
+
+if __name__ == "__main__":
+	file = "inputs/day1Input.txt"
+	day1part1(file)
+	day1part2(file)
